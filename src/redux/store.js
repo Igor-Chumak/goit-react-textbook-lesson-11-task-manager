c1054fa45b4bbb52341d5f19f934be53f3ca755a
Igor <125488812+Igor-Chumak@users.noreply.github.com>
2023-08-19 01:59:21 +0300

    start toolkit

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,12 @@
-import { devToolsEnhancer } from '@redux-devtools/extension';
-import { createStore } from 'redux';
-import { rootReducer } from './reducer';
+// import { devToolsEnhancer } from '@redux-devtools/extension';
+// import { createStore } from 'redux';
+// import { rootReducer } from './reducer';
 
-const enhancer = devToolsEnhancer();
-export const store = createStore(rootReducer, enhancer);
+// const enhancer = devToolsEnhancer();
+// export const store = createStore(rootReducer, enhancer);
+//
+//  Redux Toolkit
+//
+import { configureStore } from '@reduxjs/toolkit';
+import { rootReducer } from './reducer';
+export const store = configureStore({ reducer: rootReducer });
