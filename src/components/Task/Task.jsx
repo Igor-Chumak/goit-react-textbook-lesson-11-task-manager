9c386b4e09de3f3c79d4412398be070538fe041f
Igor <125488812+Igor-Chumak@users.noreply.github.com>
2023-08-19 00:03:38 +0300

    reducer

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,16 +1,18 @@
 import { useDispatch } from 'react-redux';
 import { MdClose } from 'react-icons/md';
-import { deleteTask } from 'redux/actions';
+import { deleteTask, toggleCompleted } from 'redux/actions';
 import css from './Task.module.css';
 
 export const Task = ({ task }) => {
   const dispatch = useDispatch();
   const handleDelete = () => dispatch(deleteTask(task.id));
+  const handleToggle = () => dispatch(toggleCompleted(task.id));
   return (
     <div className={css.wrapper}>
       <input
         type="checkbox"
         className={css.checkbox}
+        onChange={handleToggle}
         checked={task.completed}
       />
       <p className={css.text}>{task.text}</p>
